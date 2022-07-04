3c5003b46c4d95fe0c215ded1efa704fad32dc5f
Nolwac <livinusanayo96@gmail.com>
2022-07-04 12:37:35 +0100

    Edit run_kubernetes.sh script and add docker output log

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-# This tags and uploads an image to Docker Hub
+# This deploys and image with kubernetes
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nolwac/housing-price-predictor
 
-# Step 2
+
 # Run the Docker Hub container with kubernetes
+# kubectl run predictor-app --image=$dockerpath --port=80
+kubectl create deployment predictor-app --image=$dockerpath
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/predictor-app 8080:80
 
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -2,15 +2,17 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
+# define the required environment variables, i.e DOCKER_HUB_USERNAME and DOCKER_HUB_PASSWORD
+# or you can write each in a text file that is ignored for push to remote repositories, then cat the output as below
+# dockerpath=$(cat username.txt)/housing-price-predictor
 # Create dockerpath
-# dockerpath=<your docker ID/path>
-
-# Step 2:  
+dockerpath=${DOCKER_HUB_USERNAME}/housing-price-predictor
+ 
 # Authenticate & tag
+echo ${DOCKER_HUB_PASSWORD} | docker login --username ${DOCKER_HUB_USERNAME} --password-stdin
+docker image tag ml-predictor $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
 
